ae6c7e1f04fa63a028b86cb188712c2fd2556987
David Hecker <HECKERDW@GMAIL.COM>
2021-09-22 18:41:46 -0700

    Fixed lack of weather data display issue.

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -4,7 +4,7 @@ export default class Weather extends React.Component {
   render() {
     return (
       <>
-        {this.props.localResponse && this.props.localResponse.map((city, idx) => (<h3 key={idx}>{city.date}<br></br>{city.description}</h3>))}
+        {this.props.localResponse[2].date && this.props.localResponse.map((city, idx) => (<h3 key={idx}>{city.date}<br></br>{city.description}</h3>))}
       </>
     );
   }
