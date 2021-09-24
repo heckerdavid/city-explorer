a3349fcf821f86c2eb74d5880b8af4ad11250923
David Hecker <HECKERDW@GMAIL.COM>
2021-09-23 20:50:08 -0700

    Added labels to movies and weather, slight refactor.

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -4,7 +4,15 @@ export default class Weather extends React.Component {
   render() {
     return (
       <>
-        {this.props.localResponse[0].date && this.props.localResponse.map((city, idx) => (<h3 key={idx}>{city.date}<br></br>{city.description}</h3>))}
+        {this.props.localResponse[0].date && <h1>Local Weather</h1>}
+        {this.props.localResponse[0].date &&
+          this.props.localResponse.map((city, idx) => (
+            <h3 key={idx}>
+              {city.date}
+              <br></br>
+              {city.description}
+            </h3>
+          ))}
       </>
     );
   }
